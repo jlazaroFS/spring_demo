767aa8c6fce696e74a5b96a7f6ab0f2d9715c1ce
Jorge Lázaro Ruiz <jlazaro@futurespace.es>
2024-02-23 12:47:05 +0100

    handle HTTP GET request

--- src/main/java/com/prueba/demo/ui/controller/UserController.java
+++ src/main/java/com/prueba/demo/ui/controller/UserController.java
@@ -4,9 +4,11 @@ package com.prueba.demo.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping
-    public String getUser() {
-        return "get user was called";
+    public String getUsers(
+        @RequestParam(value="page", defaultValue = "1") int page,
+        @RequestParam(value="limit", defaultValue = "50") int limit,
+        @RequestParam(value="sort", defaultValue = "desc", required = false) String sort
+        )
+    {
+        return "get users was called with page = " + page + ", limit = " + limit + " and sort = " + sort;
+    }
+
+    @GetMapping(path = "/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "get user was called with userId = " + userId;
     }
 
     @PostMapping
