d00c03e008279c4ecf0d1cd9bc8795a49710c0c6
Jorge Lázaro Ruiz <jlazaro@futurespace.es>
2024-02-26 12:12:48 +0100

    validating requests

--- src/main/java/com/prueba/demo/ui/controller/UserController.java
+++ src/main/java/com/prueba/demo/ui/controller/UserController.java
@@ -2,37 +2,72 @@
 
 package com.prueba.demo.ui.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+
+import com.prueba.demo.ui.model.request.UserDetailsRequestModel;
+import com.prueba.demo.ui.model.response.UserRest;
 
 @RestController
-@RequestMapping("users")   // http://localhost:8080/users
+@RequestMapping("users") // http://localhost:8080/users
 public class UserController {
+
+    Map<String, UserRest> users;
 
     @GetMapping
     public String getUsers(
-        @RequestParam(value="page", defaultValue = "1") int page,
-        @RequestParam(value="limit", defaultValue = "50") int limit,
-        @RequestParam(value="sort", defaultValue = "desc", required = false) String sort
-        )
-    {
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "limit", defaultValue = "50") int limit,
+            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
         return "get users was called with page = " + page + ", limit = " + limit + " and sort = " + sort;
     }
 
-    @GetMapping(path = "/{userId}")
-    public String getUser(@PathVariable String userId) {
-        return "get user was called with userId = " + userId;
+    @GetMapping(path = "/{userId}", produces = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    })
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+        if (users.containsKey(userId)) {
+            return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 
-    @PostMapping
-    public String createUser() {
-        return "create user was called";
+    @PostMapping(consumes = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    }, produces = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    })
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail(userDetails.getEmail());
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+
+        String userId = UUID.randomUUID().toString();
+        returnValue.setUserId(userId);
+
+        if (users == null)
+            users = new HashMap<>();
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }
 
     @PutMapping
---
+++ src/main/java/com/prueba/demo/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,55 @@
+package com.prueba.demo.ui.model.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserDetailsRequestModel {
+    // FIXME: Los mensajes de error no aparecen
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 2, message = "First name must be at least 2 characters long")
+    private String firstName;
+
+    @NotNull(message = "Last name cannot be null")
+    private String lastName;
+
+    @NotNull(message = "Email cannot be null")
+    @Email
+    private String email;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, max = 16, message = "Password must be between 8 and 16 characters long")
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
