3d1601d58d5f58227e38519d86dfcf113690c6ef
Jorge Lázaro Ruiz <jlazaro@futurespace.es>
2024-02-27 11:41:25 +0100

    exception handling

--- src/main/java/com/prueba/demo/ui/controller/UserController.java
+++ src/main/java/com/prueba/demo/ui/controller/UserController.java
@@ -43,6 +43,10 @@ public class UserController {
             MediaType.APPLICATION_JSON_VALUE
     })
     public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+        String firstName = null;
+
+        int firstNameLength = firstName.length();
+
         if (users.containsKey(userId)) {
             return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
         } else {
