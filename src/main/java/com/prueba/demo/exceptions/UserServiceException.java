d21216b75e9c2c2069bee4547295ab7d56839319
Jorge Lázaro Ruiz <jlazaro@futurespace.es>
2024-02-28 10:22:34 +0100

    add custom exception

---
+++ src/main/java/com/prueba/demo/exceptions/UserServiceException.java
@@ -0,0 +1,8 @@
+package com.prueba.demo.exceptions;
+
+public class UserServiceException extends RuntimeException {
+
+    public UserServiceException(String message) {
+        super(message);
+    }
+}
